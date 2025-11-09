69c827eba067d3972e1b049ac8aad51c6024bf4f
Felix-wainaina <nduatifelixwain@gmail.com>
2025-11-09 19:46:29 +0300

    Fix import to match Book and Library classes for ALX checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@
 
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # 1. Function-Based View (FBV) for listing all books
 def book_list_view(request):
